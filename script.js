cac25a2f10b5927278bd405bcb206999f8661ac9
Ivan Kenneth Bibat <L00167791@ATU.IE>
2025-02-11 20:28:23 +0000

    upda6e

---
+++ script.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    document.body.innerHTML = '<h1>:) I love you.</h1>';
+}
+function moveButton() {
+    let button = document.getElementById("no");
+    let x = Math.random() * (window.innerWidth - 100);
+    let y = Math.random() * (window.innerHeight - 100);
+
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+
+    const messages = ["Nuh uh", "Think again pls", "Wrong", "Click yes smh", "skibidi plz...", ""]
+
+    button.innerText = messages[Math.floor(Math.random() * messages.length)];
+}
